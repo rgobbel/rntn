f2d2f0101b040e0686a6565822babd2b25fcb197
Randy Gobbel <gobbel@acm.org>
2016-04-27 12:26:19 -0700

    Cleanup for discuss topic on deadlock

---
+++ tensorflow/rntn_data.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+
+from phrase_tree import *
+from rntn_dictionary import *
+import csv
+import io
+
+PY3 = sys.version_info > (3,)
+
+if PY3:
+    import configparser as cfg
+else:
+    import ConfigParser as cfg
+
+def main(args):
+    from docopt import docopt
+    stree_name = 'STree.txt'
+    sostr_name = 'SOStr.txt'
+    sent_name = 'datasetSentences.txt'
+    args = docopt(main.__doc__, argv=args)
+    if args['--stree']:
+        stree_name = args['--stree']
+    if args['--sostr']:
+        sostr_name = args['--sostr']
+    if args['--sentences']:
+        sent_name = args['--sentences']
+    return load_trees(sostr_name, stree_name, sent_name)
+
+def read_config(name='rntn.config'):
+    config = cfg.ConfigParser()
+    config.read(name)
+    return config
+
+def load_trees(sostr_name, stree_name, sent_name):
+    with open(sent_name, 'r') as f_sent:
+        csv.register_dialect('tabsep', delimiter='\t', quoting=csv.QUOTE_NONE)
+        reader = csv.DictReader(f_sent, dialect='tabsep')
+        sentences = {(int(row['sentence_index'])-1): row['sentence'] for row in reader}
+    with open(sostr_name, 'r') as f_sostr:
+        sostrs = [line.strip().split('|') for line in list(f_sostr)]
+    with open(stree_name, 'r') as f_stree:
+        strees = [[int(x) for x in cleaned] for cleaned in
+                  [line.strip().split('|') for line in list(f_stree)]]
+    tree_data = zip(sostrs, strees)
+    return SentenceTree.load_trees(tree_data, sentences)
+
+
+def load_splits(splits_name, trees):
+    trains, valids, tests = [], [], []
+    splits_map = {'1': trains, '2': tests, '3': valids}
+    with open(splits_name, 'r') as f_splits:
+        reader = csv.DictReader(f_splits)
+        for row in reader:
+            splits_map[row['splitset_label']].append(trees[int(row['sentence_index'])-1])
+    return trains, valids, tests
+
+
+def find_orphan_phrases(sentences, dsdict):
+    unique_phrases = {tree.phrase for
+                      tree in [item for
+                               sublist in [sentence.nodes for
+                                           sentence in sentences] for
+                               item in sublist]}
+    dict_phrases = {entry.phrase for entry in dsdict}
+    orphan_phrases = {x for x in unique_phrases if x not in dict_phrases}
+    unused_phrases = {x for x in dict_phrases if x not in unique_phrases}
+    return orphan_phrases, unused_phrases
+
+
+def load_dataset(dsdir=None, sostr_name='SOStr.txt', stree_name='STree.txt',
+                 sentences_name='datasetSentences.txt', sentiment_name='sentiment_labels.txt',
+                 dict_name='dictionary.txt', splits_name='datasetSplit.txt',
+                 train_limit=None, test_limit=None, valid_limit=None):
+    if dsdir:
+        sostr_name = dsdir + '/' + sostr_name
+        stree_name = dsdir + '/' + stree_name
+        sentences_name = dsdir + '/' + sentences_name
+        dict_name = dsdir + '/' + dict_name
+        splits_name = dsdir + '/' + splits_name
+        sentiment_name = dsdir + '/' + sentiment_name
+    trees = load_trees(sostr_name, stree_name, sentences_name)
+    sentences = SentenceSet(trees)
+    dsdict = Dictionary.load(dict_name, sentiment_name)
+    trains, valids, tests = load_splits(splits_name, sentences)
+    return sentences, dsdict, trains[:train_limit], valids[:valid_limit], tests[:test_limit]
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv[1:])
---
+++ tensorflow/rntn_dictionary.py
@@ -0,0 +1,117 @@
+from __future__ import absolute_import
+import csv
+import io
+import sys
+
+PY3 = sys.version_info > (3,)
+
+
+class DictItem:
+
+    def __init__(self, phrase, phrase_id, sentiment=None):
+        self._phrase = phrase
+        self._phrase_id = phrase_id
+        self._sentiment = sentiment
+
+    @property
+    def phrase_id(self):
+        return self._phrase_id
+
+    @property
+    def phrase(self):
+        return self._phrase
+
+    @property
+    def sentiment(self):
+        return self._sentiment
+
+    @sentiment.setter
+    def sentiment(self, val):
+        self._sentiment = val
+
+    @property
+    def sentiment_class(self):
+        if self.sentiment   <= 0.2: return 0
+        elif self.sentiment <= 0.4: return 1
+        elif self.sentiment <= 0.6: return 2
+        elif self.sentiment <= 0.8: return 3
+        else:                       return 4
+
+    @property
+    def sentiment_1hot(self):
+        return [1.0 if self.sentiment_class == i else 0.0 for i in range(5)]
+
+
+class barsep(csv.Dialect):
+    """Bar-separated files."""
+    delimiter = '|'
+    quoting = csv.QUOTE_NONE
+    lineterminator = '\r\n'
+csv.register_dialect('barsep', barsep)
+
+class Dictionary:
+
+    def __init__(self, items=None):
+        self._idx_dict = {}
+        self._str_dict = {}
+        if items is not None:
+            for item in items:
+                self.add(item)
+
+    def add(self, item):
+        self._idx_dict[item.phrase_id] = item
+        self._str_dict[item.phrase] = item
+        return self
+
+    def __getitem__(self, item):
+        if type(item) is int:
+            return self._idx_dict[item]
+        elif type(item) is str:
+            return self._str_dict[item]
+        else:
+            raise TypeError
+
+    def __len__(self):
+        return len(self._idx_dict)
+
+    def __iter__(self):
+        self.iter_index = 0
+        return self
+
+    def __next__(self):
+        self.iter_index += 1
+        if self.iter_index > len(self):
+            raise StopIteration
+        else:
+            return self[self.iter_index-1]
+
+    def next(self):
+        return self.__next__()
+
+    def dump(self, dict_file, sentiment_file):
+        with open(dict_file, 'w') as f_dict:
+            with open(sentiment_file, 'w') as f_sent:
+                f_sent.write('phrase ids|sentiment values\n')
+                for item in self:
+                    f_dict.write('{}|{}\n'.format(item.phrase, item.phrase_id))
+                    if item.sentiment is not None:
+                        f_sent.write('{}|{}\n'.format(item.phrase_id, item.sentiment))
+
+    @classmethod
+    def load(cls, dict_filename, sentiment_filename):
+        result = cls()
+        with open(sentiment_filename, 'r') as f_sentiment:
+            if PY3:
+                csv.register_dialect('barsep', delimiter='|', quoting=csv.QUOTE_NONE)
+            fieldnames = ['phrase ids', 'sentiment values']
+            reader = csv.DictReader(f_sentiment, dialect='barsep', fieldnames=fieldnames)
+            if PY3:
+                _ = reader.__next__()
+            else:
+                _ = reader.next()
+            sentiment_map = {row['phrase ids']: float(row['sentiment values']) for row in reader}
+        with open(dict_filename, 'r') as f_dict:
+            for line in f_dict.readlines():
+                phrase, phrase_id = line.strip().split('|')
+                result.add(DictItem(phrase, int(phrase_id), sentiment_map[phrase_id]))
+        return result
